761beb840df795c9c1ed3d3498479584c3e3996d
Takuma <fuusuke0715@gmail.com>
2016-09-08 11:34:17 +0000

    Add comment scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908101958) do
+ActiveRecord::Schema.define(version: 20160908113014) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.string   "body"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
